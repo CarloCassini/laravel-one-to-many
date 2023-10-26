e89e348d66a360caba304e16954585f204a5d501
CarloCassini <c.cassini87@gmail.com>
2023-10-26 18:20:48 +0200

    cool faker

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Type;
 // importo il modello dei progetti
 use App\Models\Project;
 
@@ -24,11 +25,18 @@ class ProjectSeeder extends Seeder
     public function run(Faker $faker)
     {
 
+        $types = Type::all()->pluck('id');
+
         for ($i = 0; $i < 10; $i++) {
+
+            $type_id = $faker->randomElement($types);
 
             $project = new Project();
             $project->name = $faker->sentence(2, true);
-            $project->type_id = $faker->boolean(60) ? $faker->numberBetween(1, 3) : null;
+
+            // $project->type_id = $type_id;
+            // $project->type_id = $faker->boolean(50) ? $faker->numberBetween(1, 3) : null;
+            $project->type_id = $faker->boolean(60) ? $type_id : null;
             $project->slug = Str::slug($project->name);
             $project->git_url = $faker->url();
             $project->description = $faker->paragraph(2, true);
