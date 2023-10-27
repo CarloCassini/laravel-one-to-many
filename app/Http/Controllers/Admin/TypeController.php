69b793bb7108622f9466345765297f105157e216
CarloCassini <c.cassini87@gmail.com>
2023-10-27 15:07:04 +0200

    delete on types - end crud

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -98,6 +98,7 @@ class TypeController extends Controller
      */
     public function destroy(Type $type)
     {
-        //
+        $type->delete();
+        return redirect()->route("admin.types.index");
     }
 }
